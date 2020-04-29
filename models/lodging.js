293c9b319e5f32fdff63825d9ed4bfdff6d1ca3a
Rob Hess <robwhess@gmail.com>
2020-04-29 15:25:26 -0700

    Add code from 4/29 lecture.

--- models/lodging.js
+++ models/lodging.js
@@ -3,6 +3,7 @@
  */
 
 const { extractValidFields } = require('../lib/validation');
+const { getDBReference } = require('../lib/mongo');
 
 /*
  * Schema for a lodging.
@@ -16,4 +17,44 @@ exports.LodgingSchema = {
   zip: { required: true },
   price: { required: true },
   ownerid: { required: true }
+};
+
+exports.getLodgingsPage = async function (page) {
+  const pageSize = 10;
+
+  const db = getDBReference();
+  const collection = db.collection('lodgings');
+
+  const count = await collection.countDocuments();
+  const lastPage = Math.ceil(count / pageSize);
+  page = page > lastPage ? lastPage : page;
+  page = page < 1 ? 1 : page;
+  const offset = (page - 1) * pageSize;
+
+  const results = await collection.find({})
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(pageSize)
+    .toArray();
+
+  return {
+    lodgings: results,
+    page: page,
+    totalPages: lastPage,
+    pageSize: pageSize,
+    count: count
+  };
+};
+
+exports.insertNewLodging = async function (lodging) {
+  const validatedLodging = extractValidFields(
+    lodging,
+    exports.LodgingSchema
+  );
+
+  const db = getDBReference();
+  const collection = db.collection('lodgings');
+  const result = await collection.insertOne(validatedLodging);
+
+  return result.insertedId;
 };
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const logger = require('./lib/logger');
 const { validateAgainstSchema } = require('./lib/validation');
-const { LodgingSchema } = require('./models/lodging');
+const { connectToDB } = require('./lib/mongo');
+const { getLodgingsPage, insertNewLodging, LodgingSchema } = require('./models/lodging');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -13,20 +14,39 @@ app.use(express.json());
 
 app.use(logger);
 
-app.get('/lodgings', (req, res) => {
-  res.status(200).send({
-    lodgings: lodgings
-  });
+app.get('/lodgings', async (req, res) => {
+  try {
+    const lodgingsPage = await getLodgingsPage(
+      parseInt(req.query.page) || 1
+    );
+    res.status(200).send(lodgingsPage);
+  } catch (err) {
+    console.error(" -- error:", err);
+    res.status(500).send({
+      error: "Error fetching lodgings page."
+    });
+  }
+  // res.status(200).send({
+  //   lodgings: lodgings
+  // });
 });
 
-app.post('/lodgings', (req, res, next) => {
+app.post('/lodgings', async (req, res, next) => {
   console.log("  -- req.body:", req.body);
   if (validateAgainstSchema(req.body, LodgingSchema)) {
-    lodgings.push(req.body);
-    const id = lodgings.length - 1;
-    res.status(201).send({
-      id: id
-    });
+    // lodgings.push(req.body);
+    // const id = lodgings.length - 1;
+    try {
+      const id = await insertNewLodging(req.body)
+      res.status(201).send({
+        id: id
+      });
+    } catch (err) {
+      console.error(" -- error:", err);
+      res.status(500).send({
+        error: "Error inserting lodging."
+      });
+    }
   } else {
     res.status(400).send({
       err: "Request body does not contain a valid Lodging."
@@ -75,6 +95,8 @@ app.use('*', (req, res, next) => {
   });
 });
 
-app.listen(port, () => {
-  console.log("== Server is listening on port:", port);
+connectToDB(() => {
+  app.listen(port, () => {
+    console.log("== Server is listening on port:", port);
+  });
 });
